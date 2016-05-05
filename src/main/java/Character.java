866f314be707e5d62cca7be759847033adbc83c5
iamwho1123 <iamwho1123@gmail.com>
2016-05-05 21:58:46 +0800

    character constructer

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,18 +1,28 @@
 package main.java;
 
 import processing.core.PApplet;
+import java.awt.Color;
 
 /**
 * This class is used to store states of the characters in the program.
 * You will need to declare other variables depending on your implementation.
 */
 public class Character {
-	//8787   test
-	private MainApplet parent;
 
-	public Character(MainApplet parent){
+	private MainApplet parent;
+	
+	private String name;
+	private int value;
+	private Color c;
+	
+	private int centerX;
+	private int centerY;
+	
+	public Character(MainApplet parent , String name , int value ){
 
 		this.parent = parent;
+		this.name = name;
+		this.value = value;
 		
 	}
 
