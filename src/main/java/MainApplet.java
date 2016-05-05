a9167aaf6e0dd86e948afeba69da80c9b7ddec4f
joyce09954 <joyce09954@gmail.com>
2016-05-05 22:21:11 +0800

    loadData

--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,6 +1,9 @@
 package main.java;
 
+import java.util.ArrayList;
 import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
 
 /**
 * This class is for sketching outcome using Processing
@@ -8,13 +11,29 @@ import processing.core.PApplet;
 */
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
+	
+	JSONObject data;
+	JSONArray nodes, links;
+	
 	private String path = "main/resources/";
-	private String file = "starwars-episode-1-interactions.json";
+	private String[] str;
+	private String file1 = "starwars-episode-1-interactions.json";
+	private ArrayList<ArrayList> a1;
+	private ArrayList<Character> characters1;
+	private ArrayList<Character> characters2;
+	private ArrayList<Character> characters3;
+	private ArrayList<Character> characters4;
+	private ArrayList<Character> characters5;
+	private ArrayList<Character> characters6;
+	private ArrayList<Character> characters7;
 	
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
-
+		str = new String[7];
+		a1 = new ArrayList<ArrayList>();
+		setCharacterList();
+		setInputFilename();
 		size(width, height);
 		smooth();
 		loadData();
@@ -22,11 +41,62 @@ public class MainApplet extends PApplet{
 	}
 
 	public void draw() {
-
+		
 	}
-
+	
+	private void setCharacterList(){
+		characters1 = new ArrayList<Character>();
+		characters2 = new ArrayList<Character>();
+		characters3 = new ArrayList<Character>();
+		characters4 = new ArrayList<Character>();
+		characters5 = new ArrayList<Character>();
+		characters6 = new ArrayList<Character>();
+		characters7 = new ArrayList<Character>();
+		this.a1.add(characters1);
+		this.a1.add(characters2);
+		this.a1.add(characters3);
+		this.a1.add(characters4);
+		this.a1.add(characters5);
+		this.a1.add(characters6);
+		this.a1.add(characters7);
+	}
+	
+	private void setInputFilename(){
+		str[0] = "starwars-episode-1-interactions.json";
+		str[1] = "starwars-episode-2-interactions.json";
+		str[2] = "starwars-episode-3-interactions.json";
+		str[3] = "starwars-episode-4-interactions.json";
+		str[4] = "starwars-episode-5-interactions.json";
+		str[5] = "starwars-episode-6-interactions.json";
+		str[6] = "starwars-episode-7-interactions.json";
+	}
+	
 	private void loadData(){
-
+		for(int i=0 ; i < 7; i++){
+			System.out.println(str[i]);
+			data = loadJSONObject(path + str[i]);
+			
+			nodes = data.getJSONArray("nodes");
+			links = data.getJSONArray("links");
+			
+			for(int j=0; j< nodes.size(); j++){
+				Character c = new Character(this);
+				if(j==0)
+					this.characters1.add(c);
+				else if(j==1)
+					this.characters2.add(c);
+				else if(j == 2)
+					this.characters3.add(c);
+				else if(j == 3)
+					this.characters4.add(c);
+				else if(j == 4)
+					this.characters5.add(c);
+				else if(j == 5)
+					this.characters6.add(c);
+				else if(j == 6)
+					this.characters7.add(c);
+			}
+		}
 	}
 
 }
