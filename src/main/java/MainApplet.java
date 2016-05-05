73ee7402bb1945df8bc31fb59bd983a44ccaef7f
joyce09954 <joyce09954@gmail.com>
2016-05-05 23:09:08 +0800

    loadData

--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -18,7 +18,6 @@ public class MainApplet extends PApplet{
 	private String path = "main/resources/";
 	private String[] str;
 	private String file1 = "starwars-episode-1-interactions.json";
-	private ArrayList<ArrayList> a1;
 	private ArrayList<Character> characters1;
 	private ArrayList<Character> characters2;
 	private ArrayList<Character> characters3;
@@ -26,15 +25,21 @@ public class MainApplet extends PApplet{
 	private ArrayList<Character> characters5;
 	private ArrayList<Character> characters6;
 	private ArrayList<Character> characters7;
+	public ArrayList<Character> nowcharacters;
 	public int nowEpisode = 0;
+	public int record = 0;
 	
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
-		System.out.println("aaaaaa");
 		str = new String[7];
-		a1 = new ArrayList<ArrayList>();
-		setCharacterList();
+		characters1 = new ArrayList<Character>();
+		characters2 = new ArrayList<Character>();
+		characters3 = new ArrayList<Character>();
+		characters4 = new ArrayList<Character>();
+		characters5 = new ArrayList<Character>();
+		characters6 = new ArrayList<Character>();
+		characters7 = new ArrayList<Character>();
 		setInputFilename();
 		size(width, height);
 		smooth();
@@ -43,7 +48,13 @@ public class MainApplet extends PApplet{
 	}
 
 	public void draw() {
-		
+		this.getCharacterList();
+		if(this.record == 0){
+		for(int i=0; i< this.nowcharacters.size(); i++){
+			System.out.println(this.nowcharacters.get(i).getName());
+		}
+		this.record = 1;
+		}
 	}
 	
 	public void keyPressed(){
@@ -51,9 +62,27 @@ public class MainApplet extends PApplet{
 			nowEpisode = 0;
 		else
 			nowEpisode ++;
+		this.record = 0; 
 	}
 	
-	private void setCharacterList(){
+	private void getCharacterList(){
+		if(this.nowEpisode == 0)
+			this.nowcharacters = this.characters1;
+		else if(this.nowEpisode == 1)
+			this.nowcharacters = this.characters2;
+		else if(this.nowEpisode == 2)
+			this.nowcharacters = this.characters3;
+		else if(this.nowEpisode == 3)
+			this.nowcharacters = this.characters4;
+		else if(this.nowEpisode == 4)
+			this.nowcharacters = this.characters5;
+		else if(this.nowEpisode == 5)
+			this.nowcharacters = this.characters6;
+		else if(this.nowEpisode == 6)
+			this.nowcharacters = this.characters7;
+	}
+	
+	/*private void setCharacterList(){
 		characters1 = new ArrayList<Character>();
 		characters2 = new ArrayList<Character>();
 		characters3 = new ArrayList<Character>();
@@ -68,7 +97,7 @@ public class MainApplet extends PApplet{
 		this.a1.add(characters5);
 		this.a1.add(characters6);
 		this.a1.add(characters7);
-	}
+	}*/
 	
 	private void setInputFilename(){
 		str[0] = "starwars-episode-1-interactions.json";
@@ -89,20 +118,19 @@ public class MainApplet extends PApplet{
 			
 			for(int j=0; j< nodes.size(); j++){
 				Character c = new Character(this, nodes.getJSONObject(i).getString("name"));
-				System.out.println("aaa");
-				if(j==0)
+				if(i==0)
 					this.characters1.add(c);
-				else if(j==1)
+				else if(i==1)
 					this.characters2.add(c);
-				else if(j == 2)
+				else if(i == 2)
 					this.characters3.add(c);
-				else if(j == 3)
+				else if(i == 3)
 					this.characters4.add(c);
-				else if(j == 4)
+				else if(i == 4)
 					this.characters5.add(c);
-				else if(j == 5)
+				else if(i == 5)
 					this.characters6.add(c);
-				else if(j == 6)
+				else if(i == 6)
 					this.characters7.add(c);
 			}
 		}
